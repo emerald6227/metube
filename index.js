178e17cb132399eb13a69689fd2c4962d5355a27
emerald6227 <emerald6227@naver.com>
2021-05-20 17:53:36 +0900

    First express Server Setting

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log(`Hi!`);
+const express = require('express'); // require: 프로젝트 내에서 node module을 가져온다.
+const app = express();
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
