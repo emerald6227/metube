fa2f394fe10eec9a81a8331c7e4fdf7386ebbabb
emerald6227 <emerald6227@naver.com>
2021-05-26 16:20:40 +0900

    add middleware

--- src/server.js
+++ src/server.js
@@ -4,20 +4,37 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+const protectionMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log(`Allowed, you may continue`);
+    next();
+}
+
 const handleHome = (req, res) => {
     return res.send("welcome home here.");
     // return res.end();
 }
 
-const handleLogin = (req, res) => {
-    return res.send({ message: "It is json message from login route"});
+function loginFunc(req, res) {
+    return res.send("This page is Login Page.");
 }
 
+app.use(logger);
+app.use(protectionMiddleware);
+
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/login", loginFunc);
 
 
 const handleListening = () =>
-    console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
