3f1fb26ffc002147517e41d919aa2eada39326cb
emerald6227 <emerald6227@naver.com>
2021-06-02 10:40:59 +0900

    add minxin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,32 @@
+const videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1
+    }
+]
 export const trending = (req, res, next) => {
-    res.render("home", {pageTitle: "Home"});
+    // const videos = [1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10];
+    res.render("home", {pageTitle: "Home", videos});
 }
 export const see = (req, res) => {
     // res.send(`Watch Video #${req.params.id}`);
