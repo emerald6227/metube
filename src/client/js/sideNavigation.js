52500e6389ec89bed7b242e1befb11f4096644d7
emerald6227 <emerald6227@naver.com>
2021-07-23 17:04:17 +0900

    add not login user responsive

--- src/client/js/sideNavigation.js
+++ src/client/js/sideNavigation.js
@@ -1,24 +1,29 @@
 const navBoxDiv = document.getElementById("nav-box");
 const homeContainer = document.getElementById("home-container")
 const navMenu = document.querySelectorAll(".nav-menu");
+const navMenuLogin = document.querySelector(".nav-menu__login");
 
-let isNavigationMaximum = true;
+let notLoggedInUser = false;
 
 const navMenuMemory = [navMenu[2], navMenu[3]];
+const navMenuLoginMemory = navMenuLogin;
+
 
 const minimumNavigation = () => {
-    isNavigationMaximum = false;
-
-    if (navMenu.length > 0) { // 없을시 import 때 undefined 에러발생
+    if (navMenu) {
+        if (notLoggedInUser) {
+            navMenuLogin.remove();
+        }
         navMenu[2].remove();
         navMenu[3].remove();
     }
 };
 
 const maximumNavigation = () => {
-    isNavigationMaximum = true;
-
     if (navBoxDiv) {
+        if (notLoggedInUser) {
+            navBoxDiv.append(navMenuLoginMemory);
+        }
         navBoxDiv.append(navMenuMemory[0], navMenuMemory[1]);
     }
 };
@@ -34,6 +39,14 @@ const handleResizeScreen = () => {
         }
     }
 }
+
+const init = () => {
+    if (navMenuLogin) {
+        notLoggedInUser = true;
+    }
+}
+
+init();
 
 window.addEventListener("resize", handleResizeScreen);
 window.addEventListener("load", handleResizeScreen);
